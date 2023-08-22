c3903edcc0c644a23a4bc06c1ee517ed7bf189d0
jeissonbaquero <142328060+jeissonbaquero@users.noreply.github.com>
2023-08-22 09:58:45 -0500

    Add files via upload

---
+++ archivo/cuarto.py
@@ -0,0 +1,6 @@
+#generamos una operaicon  el cual sera impreso solo el resultado
+print(25*68+13/28)
+#podemos de utilizar paraentesis para agrupar operaciones y no solo eso sino que como en matematica est operaciones se hara primero antes que el resto
+print((25*68)+ (13/28))
+print(25*(68+13)/28)
+print((25*(68+13)+28)/28)
---
+++ archivo/tercero.py
@@ -0,0 +1,14 @@
+#primero se creo una varaible comidad la cual tiene un valor de tipo str el cual sera luego impreso llamando a la varible 
+comida="un mufin ingles"
+print ("desayuno: ")
+print(comida)
+#ahra actualizaremos la varable dandole otro nuevo valor de nuevo str el cual sera luego impreso llamando a la varible 
+comida="un plato colombiano"
+print ("almuerzo: ")
+print(comida)
+
+comida=4
+print ("comida: ")
+print(comida)
+
+
