fc115e4bec2abc138d98097d5103714a839f56c6
jeissonbaquero <142328060+jeissonbaquero@users.noreply.github.com>
2023-08-27 16:37:14 -0500

    Add files via upload

---
+++ archivo/septimo.py
@@ -0,0 +1,10 @@
+# en este codigo se realiza la operacion del modulo que consiste en
+# que no esntre el restante de una divicion
+#29 modulo de 5 (es el restante de dividir 29 entre 5 el cual es 1)
+print(29 % 5)
+#32 modulo de 3 (es el restante de dividir 32 entre 3 el cual es 2)
+print(32 % 3)
+#44 modulo de 2 (es el restante de dividir 44 entre 2 el cual es 0)
+print(44 % 2)
+#3 modulo de 44 (es el restante de dividir 3 entre 44)
+print(3 % 44)
