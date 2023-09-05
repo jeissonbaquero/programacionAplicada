4ddcfc7c3c9635964327387e380d5bdb7ab656a7
jeissonbaquero <142328060+jeissonbaquero@users.noreply.github.com>
2023-09-05 09:00:47 -0500

    Delete archivo directory

--- archivo/segundo.py
+++
@@ -1,6 +0,0 @@
-año=2023
-dia=3
-numDec=4.5
-print(type(año))
-print(type(dia))
-print(type(numDec))
--- archivo/sexto.py
+++
@@ -1,9 +0,0 @@
-#este codigo realisa la potencia del primernumeor elvandolo con respecto al segundo , esta potenica puede ser entera o fraccionario
-#2 elevado a la 10 
-print(2**10)
-#8 elevado a la 2
-print(8**2)
-# 9 elevado a la 3
-print(9**3)
-#4 elevado a la 0.5
-print(4**0.5)
