0f9de3b9557f6d51e44fb6a9c2918ea7acbe6541
jeissonbaquero <142328060+jeissonbaquero@users.noreply.github.com>
2023-11-24 05:26:52 -0500

    Create NumPyArrayIndexing.py

---
+++ Corte2/Numpy/NumPyArrayIndexing.py
@@ -0,0 +1,46 @@
+#para acceder a un lememto se debede escribir la ubicaicon que este tenga en el arreglo
+#acceder a una matris 1d
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[0])
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[1])
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[2] + arr[3])
+
+#acceder a una matris 2d
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('2nd element on 1st row: ', arr[0, 1])
+
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('5th element on 2nd row: ', arr[1, 4])
+
+#acceder a una matris 3d
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(arr[0, 1, 2])
+
+#indexasion negativa
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('Last element from 2nd dim: ', arr[1, -1])
